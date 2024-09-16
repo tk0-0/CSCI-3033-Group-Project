848efa87659cd59e4c35b70f6c325da9314875df
tk0-0 <180413722+tk0-0@users.noreply.github.com>
2024-09-15 23:26:03 -0500

    Testing Force Push Feature

--- Input.java
+++ Input.java
@@ -1,7 +1,23 @@
+import java.util.Scanner; 
+
 public class Input {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World Again!");
-        System.out.println("Hello World Again Again!");
+        int run = 0; 
+        int userOption;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Personal Budget Manager");
+        
+        while (run >= 0)
+        {
+            System.out.print("Select an option or enter -1 to exit: "); 
+            userOption = input.nextInt();
+
+            if (userOption == -1)
+                run = -1;
+        }
+
+        input.close();
     }
 }
