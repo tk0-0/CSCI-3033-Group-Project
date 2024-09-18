caa8eca9b113369261349ee722e55b27735716e1
tk0-0 <180413722+tk0-0@users.noreply.github.com>
2024-09-18 10:11:12 -0500

    Started Input Class

--- Input.java
+++ Input.java
@@ -2,22 +2,50 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        int run = 0; 
-        int userOption;
+        Scanner scan = new Scanner(System.in);
 
-        Scanner input = new Scanner(System.in);
+        int payPeriod = 0;
+        double income = 0.0;
+        double monthlyAverage = 0.0;
 
-        System.out.println("Personal Budget Manager");
+        final int maxEntries = 100;
+        String expenseDes[] = new String[maxEntries];
+        String expenseAmounts[] = new String[maxEntries];
+
+        System.out.println("Welcome to the Personal Budget Manager!\n");
         
-        while (run >= 0)
-        {
-            System.out.print("Select an option or enter -1 to exit: "); 
-            userOption = input.nextInt();
+        System.out.print("Please enter your pay period.\n0 for bi-weekly, 1 for monthly, 2 for yearly: "); 
+        payPeriod = scan.nextInt();
 
-            if (userOption == -1)
-                run = -1;
+        System.out.print("\nPlease enter your income: "); 
+        income = scan.nextDouble();
+
+        monthlyAverage = calculateMonthlyAverage(payPeriod, income);
+
+        System.out.println("Pay Period: " + payPeriod);
+        System.out.printf("Income: $%.2f\n", income);
+        System.out.printf("Monthly Average: $%.2f\n", monthlyAverage);
+
+        scan.close();
+    }
+
+    static double calculateMonthlyAverage(final int payPeriod, final double income)
+    {
+        double monthlyAverage = 0.0;
+
+        switch(payPeriod)
+        {
+            case 0:
+                monthlyAverage = income * 2;
+                break;
+            case 1:
+                monthlyAverage = income;
+                break;
+            case 2:
+                monthlyAverage = income / 12;
+                break;
         }
 
-        input.close();
+        return monthlyAverage;
     }
 }
