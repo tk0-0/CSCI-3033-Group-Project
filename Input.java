8c46c6b832674a467d664b14ec1380120bad27ff
tk0-0 <180413722+tk0-0@users.noreply.github.com>
2024-09-22 17:50:54 -0500

    Update Input.java

--- Input.java
+++ Input.java
@@ -8,9 +8,13 @@ public class Input {
         double income = 0.0;
         double monthlyAverage = 0.0;
 
+        String expenseDescription;
+        double expenseAmount;
+        int entries = 0;
+
         final int maxEntries = 100;
         String expenseDes[] = new String[maxEntries];
-        String expenseAmounts[] = new String[maxEntries];
+        double expenseAmounts[] = new double[maxEntries];
 
         System.out.println("Welcome to the Personal Budget Manager!\n");
         
@@ -20,16 +24,44 @@ public class Input {
         System.out.print("\nPlease enter your income: "); 
         income = scan.nextDouble();
 
+        System.out.println();
+
+        do {
+            System.out.print("Enter expense description (or 'done' to finish): ");
+            scan.nextLine();
+            expenseDescription = scan.nextLine();
+
+            if(expenseDescription.compareTo("done") == 0)
+                break;
+
+            expenseDes[entries] = expenseDescription;
+
+            do {
+                System.out.print("Enter expense amount (in dollars): ");
+
+                expenseAmount = scan.nextDouble();
+
+                if(expenseAmount < 0)
+                    System.out.println("Error: expense amount must be >= 0");
+
+            } while(expenseAmount < 0);
+
+            expenseAmounts[entries] = expenseAmount;
+
+            entries++;
+
+        } while(true);
+
         monthlyAverage = calculateMonthlyAverage(payPeriod, income);
 
-        System.out.println("Pay Period: " + payPeriod);
+        System.out.println("\nPay Period: " + payPeriod);
         System.out.printf("Income: $%.2f\n", income);
         System.out.printf("Monthly Average: $%.2f\n", monthlyAverage);
 
         scan.close();
     }
 
-    static double calculateMonthlyAverage(final int payPeriod, final double income)
+    public static double calculateMonthlyAverage(final int payPeriod, final double income)
     {
         double monthlyAverage = 0.0;
 
