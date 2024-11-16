e868bf3b3a7845c06bdbadf2152356748032ba92
henryngo615 <henryngo615@gmail.com>
2024-11-16 09:14:30 -0600

    Update Henry.java

---
+++ Henry.java
@@ -0,0 +1,39 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.shape.*;
+
+import javafx.scene.control.*; 
+
+// import for images; 
+import javafx.scene.image.*; 
+
+// import for text, font,etc
+import javafx.scene.text.*; 
+
+import javafx.event.*;
+
+
+
+public class Henry extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
